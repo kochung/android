a44623e63b3a2e99c2e4ee7ae47dce7e8a1c26cc
kochung <kochung3@yahoo.com.hk>
2013-03-30 18:55:45 +0800

    Some Gson implementation

--- KeigoDictAlpha/src/de/mc/keigodictalpha/MainActivity.java
+++ KeigoDictAlpha/src/de/mc/keigodictalpha/MainActivity.java
@@ -13,12 +13,16 @@ import java.net.URL;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.InputStream;
+import java.io.Reader;
 import java.io.BufferedReader;
+
+import com.google.gson.Gson;
 
 public class MainActivity extends Activity {
 	private EditText inputText;
 	private EditText sonkeigo;
 	private EditText kenjogo;
+	private String urlQuery;
 	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -39,12 +43,19 @@ public class MainActivity extends Activity {
 		String inputString = inputText.getText().toString();
 		sonkeigo.setText(inputString);
 		kenjogo.setText(inputString);
-
+		
+		urlQuery = "https://reportsapi.zoho.com/api/kochung/KeigoDict/plainList?ZOHO_ACTION=EXPORT&ZOHO_OUTPUT_FORMAT=JSON&ZOHO_ERROR_FORMAT=XML&ZOHO_API_KEY=8482375e9dc9494e26dfdfd942dc0425&ZOHO_API_VERSION=1.0";
 		try {
-			URL url = new URL("https://reportsapi.zoho.com/api/kochung/KeigoDict/plainList?ZOHO_ACTION=EXPORT&ZOHO_OUTPUT_FORMAT=JSON&ZOHO_ERROR_FORMAT=XML&ZOHO_API_KEY=8482375e9dc9494e26dfdfd942dc0425&ZOHO_API_VERSION=1.0");
+			URL url = new URL(urlQuery);
 			HttpURLConnection con = (HttpURLConnection) url
 					.openConnection();
-			readStream(con.getInputStream());
+			//readStream(con.getInputStream());
+			
+			InputStream source = con.getInputStream();
+			Gson gson = new Gson();
+			Reader reader = new InputStreamReader(source);
+			//gson.fromJson(reader, );
+			
 		} catch (Exception e) {
 	  		e.printStackTrace();
 	  	}
